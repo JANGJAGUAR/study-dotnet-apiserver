16d19d2a281d7cbf82ff73ce9f1832f2d082f330
JANGJAGUAR <jangjaguar@gmail.com>
2024-04-20 23:53:25 +0900

    fix: Hive Server

--- HiveServer/HiveServer/Models/RedisDB.cs
+++
@@ -1,9 +0,0 @@
-namespace HiveServer.Models;
-
-public class RedisDB
-{
-    // public string Email { get; set; } = "";
-    public long AccountId { get; set; } = 0;
-    public string AuthToken { get; set; } = "";
-    // public string State { get; set; } = ""; // enum UserState    
-}
